791dd0cdf9a50b928a6dba702fc34c266d843991
Anthony Scatchell <ascatche@thoughtworks.com>
2016-09-19 20:30:16 +0100

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+
+	public float speed = 15;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		if (Input.GetKey (KeyCode.Q) && speed < 100) {
+			speed += 1;
+		}
+
+		if (Input.GetKey (KeyCode.E) && speed > 0) {
+			speed -= 1;
+		}
+
+		rb.AddForce (movement * speed);
+	}
+}
