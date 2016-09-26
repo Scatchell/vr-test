63042ac3bc247e19de9fcf833066a67431edf699
Anthony <scatchell@gmail.com>
2016-09-26 21:35:48 +0100

    Anthony - Player can now dodge auto generated bullets!

---
+++ Assets/FireBullets.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBullets : MonoBehaviour {
+	public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnBullet", 3f, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnBullet () {
+		Instantiate (bullet);
+	}
+}
---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+	public bool fire = true;
+	private float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fire) {
+			gameObject.transform.position += new Vector3 (0, 0, 1) * Time.deltaTime * speed;
+		}
+	}
+
+	void OnCollisionEnter(Collision otherObj) {
+		if (otherObj.gameObject.tag == "Wall") {
+			Destroy(gameObject);
+		}
+	}
+}
